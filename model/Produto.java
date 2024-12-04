3af85fc1855b3b58ce0e349605e9a286a3e6868f
Rute-r <rute.s.r.sousa@gmail.com>
2024-12-04 16:46:09 -0300

    Commit da atividade Atividade prática – Desenvolvendo o Backend com Spring

---
+++ model/Produto.java
@@ -0,0 +1,82 @@
+package com.generation.lojagames.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatorio!")
+	@Size(min = 3, max = 30, message = "O atributo nome deve conter no maximo 30 caracteres")
+	private String nome;
+	
+	@NotNull(message = "O atributo valor é obrigatorio!")
+	@Positive(message = "O valor de preco dever ser positivo!")
+	private Float preco;
+	
+	private Boolean disponibilidade;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Float preco) {
+		this.preco = preco;
+	}
+
+	public Boolean getDisponibilidade() {
+		return disponibilidade;
+	}
+
+	public void setDisponibilidade(Boolean disponibilidade) {
+		this.disponibilidade = disponibilidade;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+	
+
+}
